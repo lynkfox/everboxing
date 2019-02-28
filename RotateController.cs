599a46634d6067c90d2db279e67c0968214c1a74
Lynkfox <lynkfox@gmail.com>
2019-02-27 23:31:59 -0500

    Code cleanup changes

--- RotateController.cs
+++ RotateController.cs
@@ -39,90 +39,100 @@ public class RotateController : MonoBehaviour
     {
         if (!levelRotating)
         {
-            
-            if (Input.GetAxisRaw("Rotate") > 0)
-            {
-                //Debug.Log("Clockwise");
-                //clockwise rotation via the e key
-                rotatingClockwise = true; // set the direction flag - These direction flags are currently unusued, but are available in case needed for other classes (EnemyController?)
-                levelRotating = true;
-                rotateCount--;
-                toAngle = transform.rotation * Quaternion.AngleAxis(rotationDegree, -transform.forward);
-                 
-            }
-            else if (Input.GetAxisRaw("Rotate") < 0)
-            {
-                //Debug.Log("Counterclockwise");
-                //counterclockwise rotation via the q key
-                rotatingCounterClock = true;
-                levelRotating = true;
-                rotateCount++;
-                toAngle = transform.rotation * Quaternion.AngleAxis(rotationDegree, transform.forward);
-                
-            }
+
+            RotateDirection();
 
         }
         else // levelRotationg = true;
         {
-            // set the start angle. Using variable so we can hold it for later if need be. Currently Unused.
-            startAngle = transform.rotation;
-
-
-            /* The actual rotate. This figures out Clockwise and Counterclockwise by switching the axis of the rotate. Forward rotates counter clockwise (Left Handed Cordinate System
-             * means that X -> Y.) -Forward will routate clockwise. This is done in the toAngle calculation above.
-             */
-
-
-            // Bug Possiblity: If the level locks up after X Rotations (of some high value of X) in the same direction, this if statement may be the culprit. Check to see if it is returning
-            // false even when the rotations "equal" - it shouldn't, not after changing to Lerp from transform.rotate, but ... The working of this if statement has been the issue for a long time.
-            if(transform.rotation != toAngle)   
-            {
-                player.transform.SetParent(this.transform); // adds the player object to the level, so it will rotate with the level and not get tossed out
-
-
-                //use Lerp to adjust the Transform - much cleaner, allows for more consistant rotation (No jerks at end) and with the error correction below, SHOULD prevent floating point errors
-                // in the above if statement (where despite appearing for all purposes equal, transform.rotation != toAngle continues to report that they are not equal)
-                transform.rotation = Quaternion.Lerp(transform.rotation, toAngle, rotateSpeed / 100); 
-                
-
-            }
-            else //Cleanup
-            {
-                
-                if( rotateCount < 360/rotationDegree)
-                {
-
-                    transform.rotation = toAngle;
-
-                    
-                } else //destination rotate count is greater than or equal to the amount of times the degRotate can divide into a circle)
-                {
-                
-                    transform.rotation = Quaternion.identity; // if we hit 360 degrees, we've made a full circle. Quaternion.identity will make sure it is zero it out.
-
-                    rotateCount = 0;
-                    /* Note:
-                     * 
-                     * Currently no Error checking if rotationDegree is not a whole divisor of 360. Error checking will be needed in awake
-                     */
-                }
-
-                rotatingCounterClock = false;
-                rotatingClockwise = false;
-                levelRotating = false;
-                player.transform.parent = null;
-                if (rotationDegree == 90)
-                    upDegree = rotationDegree;
-                else if(rotationDegree < 90)
-                {
-                    upDegree = (90 / rotationDegree - 1) * rotationDegree;
-                }
-
-                currentUp = transform.rotation * transform.rotation * Quaternion.AngleAxis(upDegree, transform.forward); // sets the curr
-            }
+            FinishRotating();
         }
         
     }
     
+    private void RotateDirection()
+    {
+        if (Input.GetAxisRaw("Rotate") > 0)
+        {
+            //Debug.Log("Clockwise");
+            //clockwise rotation via the e key
+            rotatingClockwise = true; // set the direction flag - These direction flags are currently unusued, but are available in case needed for other classes (EnemyController?)
+            levelRotating = true;
+            rotateCount--;
+            toAngle = transform.rotation * Quaternion.AngleAxis(rotationDegree, -transform.forward);
+
+        }
+        else if (Input.GetAxisRaw("Rotate") < 0)
+        {
+            //Debug.Log("Counterclockwise");
+            //counterclockwise rotation via the q key
+            rotatingCounterClock = true;
+            levelRotating = true;
+            rotateCount++;
+            toAngle = transform.rotation * Quaternion.AngleAxis(rotationDegree, transform.forward);
+
+        }
+    }
+
+    private void FinishRotating()
+    {
+        // set the start angle. Using variable so we can hold it for later if need be. Currently Unused.
+        startAngle = transform.rotation;
+
+
+        /* The actual rotate. This figures out Clockwise and Counterclockwise by switching the axis of the rotate. Forward rotates counter clockwise (Left Handed Cordinate System
+         * means that X -> Y.) -Forward will routate clockwise. This is done in the toAngle calculation above.
+         */
+
+
+        // Bug Possiblity: If the level locks up after X Rotations (of some high value of X) in the same direction, this if statement may be the culprit. Check to see if it is returning
+        // false even when the rotations "equal" - it shouldn't, not after changing to Lerp from transform.rotate, but ... The working of this if statement has been the issue for a long time.
+        if (transform.rotation != toAngle)
+        {
+            player.transform.SetParent(this.transform); // adds the player object to the level, so it will rotate with the level and not get tossed out
+
+
+            //use Lerp to adjust the Transform - much cleaner, allows for more consistant rotation (No jerks at end) and with the error correction below, SHOULD prevent floating point errors
+            // in the above if statement (where despite appearing for all purposes equal, transform.rotation != toAngle continues to report that they are not equal)
+            transform.rotation = Quaternion.Lerp(transform.rotation, toAngle, rotateSpeed / 100);
+
+
+        }
+        else //Cleanup
+        {
+
+            if (rotateCount < 360 / rotationDegree)
+            {
+
+                transform.rotation = toAngle;
+
+
+            }
+            else //destination rotate count is greater than or equal to the amount of times the degRotate can divide into a circle)
+            {
+
+                transform.rotation = Quaternion.identity; // if we hit 360 degrees, we've made a full circle. Quaternion.identity will make sure it is zero it out.
+
+                rotateCount = 0;
+                /* Note:
+                 * 
+                 * Currently no Error checking if rotationDegree is not a whole divisor of 360. Error checking will be needed in awake
+                 */
+            }
+
+            rotatingCounterClock = false;
+            rotatingClockwise = false;
+            levelRotating = false;
+            player.transform.parent = null;
+            if (rotationDegree == 90)
+                upDegree = rotationDegree;
+            else if (rotationDegree < 90)
+            {
+                upDegree = (90 / rotationDegree - 1) * rotationDegree;
+            }
+
+            currentUp = transform.rotation * transform.rotation * Quaternion.AngleAxis(upDegree, transform.forward); // sets the curr
+        }
+    }
 }
 
--- movementController.cs
+++ movementController.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class movementController : MonoBehaviour
 {
     [SerializeField] public float speed = 5f; //public in order to allow terrain to change the speed of the character (rough/ice/ect)
-    [SerializeField] private float jumpHeight = 7f;
+    [SerializeField] protected float jumpHeight = 7f;
     [SerializeField] public GameObject level; // levels will change.
 
 
 
-    private bool jumping = false;
+    protected bool jumping = false, moving = false;
     
     private float startDegree;
 
 
-    private Rigidbody rb;
+    protected Rigidbody rb;
    
 
     private void Awake()
@@ -29,25 +29,39 @@ public class movementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // keeps the player looking in the direction of movement.
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward);
 
+        
+        Jump();
+        Move();
+    }
+
+    private void Move()
+    {
+        if(!moving)
+        { 
+        
+            // keeps the player looking in the direction of movement.
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.rotation = Quaternion.LookRotation(Vector3.forward);
+
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.rotation = Quaternion.LookRotation(-Vector3.forward);
+
+            }
+
+            // Moves the player.
+            float move = Input.GetAxisRaw("Horizontal");
+            Vector3 movement = new Vector3(move, 0, 0);
+            //rb.AddForce(movement * speed);
+            rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
         }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            transform.rotation = Quaternion.LookRotation(-Vector3.forward);
+    }
 
-        }
-
-        // Moves the player.
-        float move = Input.GetAxisRaw("Horizontal");
-        Vector3 movement = new Vector3(move, 0, 0);
-        //rb.AddForce(movement * speed);
-        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
-
-
+    private void Jump()
+    {
         if (!jumping)
         {
             if (Input.GetButtonDown("Jump"))
@@ -65,19 +79,8 @@ public class movementController : MonoBehaviour
         jumping = false;
     }
 
-    /*
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == 10) //layer 10 = walls. If the player is currently on a wall, disable jumping. Needs a little work (very crude)
-        {
-            jumping = false;
-        }
-    }
-
-    */
     
-
 }
 
 
